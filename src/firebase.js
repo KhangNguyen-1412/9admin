c0958940af55bd46ba26faa2b0851dbd22de910f
KhangNguyen-1412 <nhpk1412@example.com>
2026-04-21 08:47:52 +0700

    feat: initialize Firebase SDK and ignore environment variable files

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBZherk8dHTOabrTfXAgSaCYW6ouwdHG7Q",
-  authDomain: "admin-12e22.firebaseapp.com",
-  projectId: "admin-12e22",
-  storageBucket: "admin-12e22.firebasestorage.app",
-  messagingSenderId: "581552531611",
-  appId: "1:581552531611:web:cb8595982a0fdbb6d87e3e",
-  measurementId: "G-5HLS560H1Z"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
